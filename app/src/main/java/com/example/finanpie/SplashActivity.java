939752ce303f593a1c2f5cf4a1f2c8a09d6812fb
EmilioSanchez99 <emiliosv_99@hotmail.com>
2025-05-13 19:37:49 +0200

    splash y publi

---
+++ app/src/main/java/com/example/finanpie/SplashActivity.java
@@ -0,0 +1,39 @@
+package com.example.finanpie;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.VideoView;
+import android.media.MediaPlayer;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final int DURACION_SPLASH = 5000; // opcional, si quieres forzar tiempo
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        VideoView videoView = findViewById(R.id.videoView);
+        Uri videoUri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.publi);
+        videoView.setVideoURI(videoUri);
+
+        videoView.setOnCompletionListener(mp -> {
+            startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+            finish();
+        });
+
+        videoView.start();
+
+        // ❗ Por si quieres forzar cambio tras X segundos
+        /*
+        new Handler().postDelayed(() -> {
+            if (videoView.isPlaying()) videoView.stopPlayback();
+            startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+            finish();
+        }, DURACION_SPLASH);
+        */
+    }
+}
